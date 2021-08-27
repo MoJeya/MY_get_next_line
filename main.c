736d9a47a6c67639d7f55df6358f68b278f034a0
Moghanavathanan Jeyavathanan <mjeyavat@1-E-9.42heilbronn.de>
2021-08-27 19:27:42 +0200

    main file only for testing

--- main.c
+++ main.c
@@ -1,17 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mjeyavat <mjeyavat@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/06 17:05:07 by mjeyavat          #+#    #+#             */
-/*   Updated: 2021/08/26 14:16:33 by mjeyavat         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-//how to write to files
-
 #include "get_next_line.h"
 
 int main()
@@ -32,45 +18,3 @@ int main()
 	system("leaks a.out");
 	return (0);
 }
-
-
-
-/**
- *
- *
-	#include <stdio.h>
-	#include <stdlib.h>
-	#include <fcntl.h>
-	#ifdef __unix__
-      #include <unistd.h>
-      #include <sys/stat.h>
-      #include <sys/types.h>
-	#elif __MSDOS__ || __WIN32__ || _MSC_VER
-      #include <io.h>
-      #include <sys\stat.h>
-	#endif
-	#define MAXBYTES 1024
-
-	int main(int argc, char *argv[]) {
-   		int in,out,count;
-   		char buffer[MAXBYTES];
-
-   		if(argc < 3) {
-      		printf("Aufruf: programmname quelldatei zieldatei\n");
-      		return EXIT_FAILURE;
-   		}
-   		if( (in=open(*++argv ,O_RDONLY)) == -1)
-      		printf("Fehler open %s\n", (char *)argv);
-   		if( (out=open(*++argv, O_WRONLY | O_TRUNC | O_CREAT)) == -1)
-      		printf("Fehler open %s\n", (char *)argv);
-   		while( (count = read(in, buffer, MAXBYTES)) )
-      		write(out,buffer,count);
-   		close(in);
-   		close(out);
-   		return EXIT_SUCCESS;
-	}
- *
- *
- *
-*/
-
